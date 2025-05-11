21c17bb4c27502a444a651f42445be3af66bbd09
ledeunf <JeanFrancois@gmail.com>
2025-05-11 17:13:02 +0200

    verbes irreguliers

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+// hamburger.js
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navUl = document.querySelector('nav ul');
+
+    hamburger.addEventListener('click', function() {
+        navUl.classList.toggle('show');
+    });
+});
